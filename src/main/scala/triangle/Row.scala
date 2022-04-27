e3db8cb7fe87ce20f625cdaf52df6e99a05c24b0
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-04-28 01:40:14 +0300

    Adds first working solution

--- src/main/scala/triangle/Row.scala
+++ src/main/scala/triangle/Row.scala
@@ -3,14 +3,18 @@ package triangle
 case class Row(values: Seq[Int])
 
 object Row {
-
   def fold(results: Seq[Result], row: Row): Seq[Result] = {
     results
       .sliding(2)
-      .toSeq
+      .toSeq // conversion will happen sooner or later
       .map(_.minBy(_.cost))
       .lazyZip(row.values)
       .map(_ + _)
+  }
+
+  def foldAll(rows: Seq[Row]): Seq[Result] = rows.headOption match {
+    case None => Seq()
+    case Some(r) => rows.tail.foldLeft(r.values.map(Result(_)))(fold)
   }
 }
 
--- src/test/scala/triangle/Row.scala
+++ src/test/scala/triangle/Row.scala
@@ -2,8 +2,9 @@ package triangle
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.LoneElement
 
-class RowTests extends AnyWordSpec with Matchers {
+class RowTests extends AnyWordSpec with Matchers with LoneElement {
 
   "Row folding" should {
 
@@ -12,6 +13,22 @@ class RowTests extends AnyWordSpec with Matchers {
       val results = Seq(Result(6), Result(7), Result(5), Result(4))
       
       Row.fold(results, row).map(_.cost) should be (Seq(9, 7, 5))
+    }
+
+  }
+
+
+  "Row.foldAll" should {
+
+    "calculate the example" in {
+      val rows = Seq(
+        Row(Seq(11,2,10,9)),
+        Row(Seq(3,8,5)),
+        Row(Seq(6,3)),
+        Row(Seq(7)),
+      )
+
+      Row.foldAll(rows).loneElement should be (Result("7 + 6 + 3 + 2", 18))
     }
 
   }
