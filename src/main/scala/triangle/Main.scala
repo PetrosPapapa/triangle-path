c0cfbce6d7777fbbe9aaaf32538db73a08e1235a
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-04-28 14:45:30 +0300

    Extends `Main` to run on random triangles of given size

--- src/main/scala/triangle/Main.scala
+++ src/main/scala/triangle/Main.scala
@@ -4,13 +4,30 @@ import cats.implicits.*
 
 import cats.effect.{ IO, IOApp, ExitCode }
 import cats.effect.implicits.*
+import cats.effect.std.Random
 
-object Main extends IOApp with Input {
+import util.Try
+
+object Main extends IOApp with Input with RandomRow {
 
   def run(args: List[String]): IO[ExitCode] =
     args.headOption match {
-      case Some(name) =>
-        IO.println(s"Hello, $name.").as(ExitCode.Success)
+ 
+     case Some(n) =>
+        Try(n.toInt).toOption match {
+          case None => IO.println("Invalid argument.").as(ExitCode.Error)
+          case Some(i) => for {
+            rand <- Random.scalaUtilRandom[IO]
+            triangle <- gen(rand, i)
+            solution = Row.foldAll(triangle)
+            code <- solution match {
+              case Seq() => IO.println("Empty triangle.").as(ExitCode.Error)
+              case Seq(r) => IO.println(r.output).as(ExitCode.Success)
+              case _ => IO.println("Invalid triangle.").as(ExitCode.Error)
+            }
+          } yield (code)
+        }
+
       case None =>
         List().tailRecM[IO, Option[List[Row]]](read)
           .flatMap( _ match {
