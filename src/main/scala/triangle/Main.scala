0e4957e0c3eb3a047aee0378b0e404f8eb3749df
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-04-28 13:54:28 +0300

    Updates `Main` to run algorithm and print solution

--- src/main/scala/triangle/Main.scala
+++ src/main/scala/triangle/Main.scala
@@ -14,8 +14,13 @@ object Main extends IOApp with Input {
       case None =>
         List().tailRecM[IO, Option[List[Row]]](read)
           .flatMap( _ match {
-            case None => IO.println("No parse.").as(ExitCode.Error)
-            case Some(s) => IO.println(s).as(ExitCode.Success)
+            case None => IO.println("Failed to parse.").as(ExitCode.Error)
+            case Some(s) =>
+              Row.foldAll(s) match {
+                case Seq() => IO.println("Empty triangle.").as(ExitCode.Error)
+                case Seq(r) => IO.println(r.output).as(ExitCode.Success)
+                case _ => IO.println("Invalid triangle.").as(ExitCode.Error)
+              }
           })
     }  
 
--- src/main/scala/triangle/Row.scala
+++ src/main/scala/triangle/Row.scala
@@ -42,9 +42,11 @@ object Row {
 
 case class Result(path: String, cost: Int) {
   def +(node: Int): Result = copy(
-    path = node.toString + " + " + path, // we are going bottom-up!
+    path = node.toString + " + " + path, // we are going bottom-up so adding to the front
     cost = cost + node
   )
+
+  def output: String = s"Minimal path is: $path = $cost"
 }
 
 object Result {
